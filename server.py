aa6055b1d46841de23ff20921446fa5bd7d26cc1
gin22 <57427735+gin22@users.noreply.github.com>
2020-07-13 20:31:35 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
-from starlette.applications import Starlette
-from starlette.responses import HTMLResponse, JSONResponse
-from starlette.staticfiles import StarticFiles
-from starlette.middleware.cors import CORSMidlleware
-import uvicorn, aiohttp, asyncio
-from io import BytesIO
-
+import aiohttp
+import asyncio
+import uvicorn
+import os
 from fastai import *
-from fastai.vision import*
+from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+import requests
 
-# Change the export_file_url. I used Google Drive link:              
-export_file_url =  'https://drive.google.com/file/d/1g0ZbeWZFgU7mL-0EmbOminZ0jeF-kUpu/view?usp=sharing'                       
-#Write the model's name:
-export_file_name = 'model_klasifikasi_tumor.pkl' 
-#Write all the classes:
-                                              
-classes = ['Tumor Ganas', 'Tumor Jinak']
+Port = int(os.environ.get('PORT', 5000))
 
+export_file_url = 'https://drive.google.com/file/d/1g0ZbeWZFgU7mL-0EmbOminZ0jeF-kUpu/view?usp=sharing'
+export_file_name = 'model_klasifikasi_tumor.pkl'
+
+classes = ["Tumor jinak" , "Tumor ganas"]
 path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+app.mount('/prod-view', StaticFiles(directory='app/prod-view'))
